5ca55aa47edd0d98020f68cfa935e9c3f8c295be
SikderMdSharifulIslam <sikdermdsharifulislam@gmail.com>
2026-02-25 22:51:28 +0600

    math functions of js

---
+++ math.js
@@ -0,0 +1,11 @@
+console.log(Math.min(1,2,3)); //will return min value
+console.log(Math.max(1,2,3)); //will return max value
+console.log(Math.abs(20-30)); //will return signless result
+console.log(Math.PI); //will return PI value
+console.log(Math.round(2.25)); //will return floor value 2
+console.log(Math.round(2.85)); //will return ceil value 3
+console.log(Math.floor(2.85)); //will return floor value 2 forcefully
+console.log(Math.ceil(2.15)); //will return ceil value 3 forcefully
+console.log(Math.random()); //will return random numbers
+console.log(Math.random()*10); //will return random numbers from 0 to 10
+console.log(Math.round(Math.random()*10)); //will return random numbers from 0 to 10 round numbers
